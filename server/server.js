84bc3c6e6e1928eeb6efca52d030813cb36a8462
Donovan McCollum <donomcc@gmail.com>
2022-09-13 21:13:25 -0500

    added css styling to header

--- server/server.js
+++ server/server.js
@@ -7,7 +7,11 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../index.html"));
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+app.get("/styles", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.css"));
 });
 
 const port = process.env.PORT || 4005;
